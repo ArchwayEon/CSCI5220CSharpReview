ac4a981ce27c830348abadfeb34b45f9ca821129
Jeff Roach <roachj@etsu.edu>
2023-09-06 10:48:13 -0400

    Array demonstration

--- CSCI5220CSharpReview/Program.cs
+++ CSCI5220CSharpReview/Program.cs
@@ -3,7 +3,8 @@
 //Console.WriteLine("Hello, World!");
 //EnumDemo();
 //ClassDemo();
-InterfaceDemo();
+//InterfaceDemo();
+ArrayDemo();
 
 static void EnumDemo()
 {
@@ -43,4 +44,34 @@ static void InterfaceDemo()
     Console.WriteLine($"The area of the shape is {r.GetArea()}");
 }
 
+static void ArrayDemo()
+{
+    // Declare a single-dimensional array  
+    int[] array1 = new int[5];
 
+    string a1str = String.Join(",", array1);
+
+    // Declare and set array element values 
+    int[] array2 = new int[] { 1, 3, 5, 7, 9 };
+
+    string a2str = String.Join(",", array2);
+
+    // Alternative syntax 
+    int[] array3 = { 1, 2, 3, 4, 5, 6 };
+
+    string a3str = String.Join(",", array3);
+
+    // Declare a two-dimensional array 
+    int[,] multiDimensionalArray1 = new int[2, 3];
+
+    // Declare and set array element values 
+    int[,] multiDimensionalArray2 = { { 1, 2, 3 }, { 4, 5, 6 } };
+
+    // Declare a jagged array 
+    int[][] jaggedArray = new int[6][];
+
+    // Set the values of the first array in the jagged array structure
+    jaggedArray[0] = new int[4] { 1, 2, 3, 4 };
+
+    Console.WriteLine($"Array 1 [{a1str}] Array 2 [{a2str}] Array 3 [{a3str}]");
+}
