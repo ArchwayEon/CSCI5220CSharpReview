8e2fb2c6370edf377c568220ea375db49fd9f718
Jeff Roach <37549850+ArchwayEon@users.noreply.github.com>
2023-09-06 06:52:47 -0400

    Add project files.

---
+++ CSCI5220CSharpReview/MyEnums.cs
@@ -0,0 +1,5 @@
+﻿namespace CSCI5220CSharpReview
+{
+    internal enum Days { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday };
+    internal enum Months : byte { Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec };
+}
---
+++ CSCI5220CSharpReview/Program.cs
@@ -0,0 +1,22 @@
+﻿using CSCI5220CSharpReview;
+
+Console.WriteLine("Hello, World!");
+EnumDemo();
+
+
+static void EnumDemo()
+{
+    Days today = Days.Monday;
+    int dayNumber = (int)today;
+    string model = $"{today} is day number #{dayNumber}.\n";
+
+    foreach (string item in Enum.GetNames(typeof(Months)))
+    {
+        model += item + " ";
+    }
+
+    Console.WriteLine(model);
+}
+
+
+
